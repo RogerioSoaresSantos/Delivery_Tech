b5a8c0ca18ce580d9fb08df739dba4ed5b85e2a9
Rogerio Soares Santos <rogerio.santos@202503061572@alunos.estacio.br>
2025-10-28 19:28:30 -0300

    Aula4 27/10/25

---
+++ src/main/java/com/deliverytech/delivery_api/dto/request/RegisterRequest.java
@@ -0,0 +1,29 @@
+package com.deliverytech.delivery_api.dto.request;
+
+import com.deliverytech.delivery_api.model.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @Email(message = "Email deve ter formato válido")
+    @NotBlank(message = "Email é obrigatório")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    private String senha;
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 2, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
+    private String nome;
+    
+    @NotBlank(message = "Papel é obrigatório")
+    private Role role;
+
+    
+}
